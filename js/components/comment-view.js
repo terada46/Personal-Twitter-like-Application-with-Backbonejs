9efa08b5d99a9e03c3780feb008c8ebb22d24e2c
terada46 <mizukiloveu@gmail.com>
2017-10-11 16:53:59 +0800

    Twitter can be add comments now (css stylling work are not done)

---
+++ js/components/comment-view.js
@@ -0,0 +1,31 @@
+var app = app || {};
+
+(function ($) {
+
+	app.CommentView = Backbone.View.extend({
+
+		tagName: 'div',
+
+		className: 'comment',
+
+		comment_template: template('comment-template'),
+
+		render: function() {
+			this.$el.html(this.comment_template(this.model.toJSON()));
+			return this;
+		}
+
+	});
+
+	app.NewCommentView = Backbone.View.extend({
+		tagName: 'li',
+
+		new_comment_template: template('new-comment'),
+
+		render: function() {
+			this.$el.html(this.new_comment_template(this.model.toJSON()));
+			return this;
+		}
+
+	})
+})(jQuery);
